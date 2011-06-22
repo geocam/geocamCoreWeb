af79b73269f39937b5497e5ddb61933532400eb8
Trey Smith <trey.smith@nasa.gov>
2011-06-22 16:24:54 -0700

    added geocamFolder

--- geocamCore/baseSettings.py
+++ geocamCore/baseSettings.py
@@ -110,6 +110,7 @@ TEMPLATE_DIRS = (
 INSTALLED_APPS = (
     'geocamCore',
     'geocamUtil',
+    'geocamFolder',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
