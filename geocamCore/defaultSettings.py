004db66248014f355e4c8279e4495bd98f792d12
Trey Smith <trey.smith@nasa.gov>
2012-04-06 09:39:17 -0700

    moved much of geocamCore code to geocamShare and shareApp

--- geocamCore/defaultSettings.py
+++ geocamCore/defaultSettings.py
@@ -3,10 +3,3 @@
 # the Administrator of the National Aeronautics and Space Administration.
 # All Rights Reserved.
 # __END_LICENSE__
-
-GEOCAM_CORE_SITE_TITLE = 'GeoCam Share'
-
-# for multiple thumbnails in sidebar gallery
-GEOCAM_CORE_GALLERY_THUMB_SIZE = [160, 120]
-# for single thumbnail in sidebar gallery
-GEOCAM_CORE_DESC_THUMB_SIZE = [480, 360]
