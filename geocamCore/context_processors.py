6b83530e0c9f544c694e9fc3467b99d3ab0b4a44
Tamar Cohen <tamar.e.cohen@nasa.gov>
2014-09-11 10:26:45 -0700

    added a siteSettings context processor

--- geocamCore/context_processors.py
+++ geocamCore/context_processors.py
@@ -9,3 +9,13 @@ from django.conf import settings as settings_
 
 def settings(request):
     return dict(settings=settings_)
+
+
+def siteSettings(request):
+    """
+    Oftentimes we will use the keyword settings to return a settings from a view, and using this context processor as siteSettings will not conflict with that pattern.
+    In this case you should NOT reference the above settings, but instead reference this siteSettings in your TEMPLATE_CONTEXT_PROCESSORS in siteSettings as follows
+    'geocamUtil.context_processors.siteSettings'
+    and in the template, use siteSettings.YOUR_SETTING_NAME.
+    """
+    return dict(siteSettings=settings_)
